2ba6f3dfb15badf11394bd6d1ab2b68d7b0bf874
김대현 <ungk456@naver.com>
2021-07-23 17:44:41 +0900

    ADD : all

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import Continents from "./Continents";
+
+
+const client = new ApolloClient({
+  uri: "https://countries.trevorblades.com",
+});
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <h1>React + Apollo Client</h1>
+      <Continents />
+    </ApolloProvider>
   );
 }
 
